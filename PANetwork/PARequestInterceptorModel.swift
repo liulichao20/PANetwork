f9c45a39e403852d7b90ca5c961c0136dba12118
lichao_liu <liulichao@lichao-liu.local>
2018-03-06 14:04:06 +0800

    网络请求拦截

--- PANetwork/PARequestInterceptorModel.swift
+++ PANetwork/PARequestInterceptorModel.swift
@@ -10,12 +10,48 @@ import UIKit
 
 class PARequestInterceptorModel: PARequestInterceptor {
     func requestInterceptor(request: PARequest) {
-         //请求拦截
+        let dict = ["version":"1.1.1","time":"2016.1.1"]
+        //请求拦截
         if request.requestMethod == .post {
-            
+            //添加参数到paramaters上
+            for (key,value) in dict {
+                request.requestParams?.updateValue(key, forKey: value)
+            }
         }else {
             //添加到url上
+            var requestUrl:String = request.requestUrl ?? ""
+            var array:[String] = []
+            for (key,value) in dict {
+                array.append("\(encode(key: key))=\(encode(key: value))")
+            }
+            let compStr = array.joined(separator: "&")
             
+            if !requestUrl.isEmpty {
+                if let url = URL(string: requestUrl),url.scheme != nil ,url.host != nil {
+                    //添加到requesturl上
+                    
+                }else {
+                    //baseurl + requesturl
+                    if let renativeUrl = URL(string: requestUrl, relativeTo: URL(string: PANetworkConfig.default.baseUrl ?? "")) {
+                        requestUrl = renativeUrl.absoluteString
+                    }
+                }
+            }else {
+                requestUrl = PANetworkConfig.default.baseUrl ?? ""
+            }
+            
+            
+            var component = URLComponents(string: requestUrl)
+            if let query = component?.query {
+                component?.query = "\(query)&\(compStr)"
+            }else {
+                component?.query = compStr
+            }
+            request.requestUrl = component?.url?.absoluteString ?? ""
         }
+    }
+    
+    func encode(key:String)->String {
+        return key.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) ?? ""
     }
 }
